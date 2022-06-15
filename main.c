62f91b03e0140ab8a612775f05f0a88b06c8909e
Isaac Soares <isaacsantos.soares@hotmail.com>
2022-06-15 10:13:06 -0300

    2 rodadas de teste empirico

--- main.c
+++ main.c
@@ -7,34 +7,35 @@
 
 int main(void) {
 
-  clock_t ciclos_ini, soma_ciclos;
+  clock_t ciclos_ini_fix = 0, ciclos_ini = 0, quant_ciclos = 0;
   FILE* tempo = fopen("./tempos.txt", "w");
 
+  ciclos_ini_fix = clock();
 
   fprintf (tempo, "---VARIA-FRAGAMENTOS---\n");
   ciclos_ini = clock();
   ContagemLeituras ("./projeto_1_dados/genoma_grande.txt", "./projeto_1_dados/pos_genes_grande.csv", "./projeto_1_dados/fragmentos_pequeno.txt", 
     "./pos_fragmentos.txt", 10000,"./atividade_genica.txt");
-  soma_ciclos =+ clock() - ciclos_ini;
+  quant_ciclos = clock() - ciclos_ini;
   fprintf (tempo, "PEQUENO\n");
-  fprintf (tempo, "Soma ciclos: %d\n", soma_ciclos);
-  fprintf (tempo, "Tempo (segundos): %.50lf\n", (((double) soma_ciclos)) / CLOCKS_PER_SEC);
+  fprintf (tempo, "Ciclos necessarios: %ld\n", quant_ciclos);
+  fprintf (tempo, "Tempo (segundos): %.50lf\n", (((double) quant_ciclos)) / CLOCKS_PER_SEC);
 
   ciclos_ini = clock();
   ContagemLeituras ("./projeto_1_dados/genoma_grande.txt", "./projeto_1_dados/pos_genes_grande.csv", "./projeto_1_dados/fragmentos_medio.txt", 
     "./pos_fragmentos.txt", 10000,"./atividade_genica.txt");
-  soma_ciclos =+ clock() - ciclos_ini;
+  quant_ciclos = clock() - ciclos_ini;
   fprintf (tempo, "MEDIO\n");
-  fprintf (tempo, "Soma ciclos: %d\n", soma_ciclos);
-  fprintf (tempo, "Tempo (segundos): %.50lf\n", (((double) soma_ciclos)) / CLOCKS_PER_SEC);
+  fprintf (tempo, "Ciclos necessarios: %ld\n", quant_ciclos);
+  fprintf (tempo, "Tempo (segundos): %.50lf\n", (((double) quant_ciclos)) / CLOCKS_PER_SEC);
 
   ciclos_ini = clock();
   ContagemLeituras ("./projeto_1_dados/genoma_grande.txt", "./projeto_1_dados/pos_genes_grande.csv", "./projeto_1_dados/fragmentos_grande.txt", 
     "./pos_fragmentos.txt", 10000,"./atividade_genica.txt");
-  soma_ciclos =+ clock() - ciclos_ini;
+  quant_ciclos = clock() - ciclos_ini;
   fprintf (tempo, "GRANDE\n");
-  fprintf (tempo, "Soma ciclos: %d\n", soma_ciclos);
-  fprintf (tempo, "Tempo (segundos): %.50lf\n", (((double) soma_ciclos)) / CLOCKS_PER_SEC);
+  fprintf (tempo, "Ciclos necessarios: %ld\n", quant_ciclos);
+  fprintf (tempo, "Tempo (segundos): %.50lf\n", (((double) quant_ciclos)) / CLOCKS_PER_SEC);
 
   fprintf (tempo, "-----------------------\n");
 
@@ -43,26 +44,26 @@ int main(void) {
   ciclos_ini = clock();
   ContagemLeituras ("./projeto_1_dados/genoma_grande.txt", "./projeto_1_dados/pos_genes_pequeno.csv", "./projeto_1_dados/fragmentos_grande.txt", 
     "./pos_fragmentos.txt", 100,"./atividade_genica.txt");
-  soma_ciclos =+ clock() - ciclos_ini;
+  quant_ciclos = clock() - ciclos_ini;
   fprintf (tempo, "PEQUENO\n");
-  fprintf (tempo, "Soma ciclos: %d\n", soma_ciclos);
-  fprintf (tempo, "Tempo (segundos): %.50lf\n", (((double) soma_ciclos)) / CLOCKS_PER_SEC);
+  fprintf (tempo, "Ciclos necessarios: %ld\n", quant_ciclos);
+  fprintf (tempo, "Tempo (segundos): %.50lf\n", (((double) quant_ciclos)) / CLOCKS_PER_SEC);
 
   ciclos_ini = clock();  
   ContagemLeituras ("./projeto_1_dados/genoma_grande.txt", "./projeto_1_dados/pos_genes_medio.csv", "./projeto_1_dados/fragmentos_grande.txt", 
     "./pos_fragmentos.txt", 1000,"./atividade_genica.txt");
-  soma_ciclos =+ clock() - ciclos_ini;
+  quant_ciclos = clock() - ciclos_ini;
   fprintf (tempo, "MEDIO\n");
-  fprintf (tempo, "Soma ciclos: %d\n", soma_ciclos);
-  fprintf (tempo, "Tempo (segundos): %.50lf\n", (((double) soma_ciclos)) / CLOCKS_PER_SEC);
+  fprintf (tempo, "Ciclos necessarios: %ld\n", quant_ciclos);
+  fprintf (tempo, "Tempo (segundos): %.50lf\n", (((double) quant_ciclos)) / CLOCKS_PER_SEC);
 
   ciclos_ini = clock();
   ContagemLeituras ("./projeto_1_dados/genoma_grande.txt", "./projeto_1_dados/pos_genes_grande.csv", "./projeto_1_dados/fragmentos_grande.txt", 
     "./pos_fragmentos.txt", 10000,"./atividade_genica.txt");
-  soma_ciclos =+ clock() - ciclos_ini;
+  quant_ciclos = clock() - ciclos_ini;
   fprintf (tempo, "GRANDE\n");
-  fprintf (tempo, "Soma ciclos: %d\n", soma_ciclos);
-  fprintf (tempo, "Tempo (segundos): %.50lf\n", (((double) soma_ciclos)) / CLOCKS_PER_SEC);
+  fprintf (tempo, "Ciclos necessarios: %ld\n", quant_ciclos);
+  fprintf (tempo, "Tempo (segundos): %.50lf\n", (((double) quant_ciclos)) / CLOCKS_PER_SEC);
 
   fprintf (tempo, "---------------------\n");
 
@@ -71,28 +72,32 @@ int main(void) {
   ciclos_ini = clock();
   ContagemLeituras ("./projeto_1_dados/genoma_pequeno.txt", "./projeto_1_dados/pos_genes_grande.csv", "./projeto_1_dados/fragmentos_grande.txt", 
     "./pos_fragmentos.txt", 10000,"./atividade_genica.txt");
-  soma_ciclos =+ clock() - ciclos_ini;
+  quant_ciclos = clock() - ciclos_ini;
   fprintf (tempo, "PEQUENO\n");
-  fprintf (tempo, "Soma ciclos: %d\n", soma_ciclos);
-  fprintf (tempo, "Tempo (segundos): %.50lf\n", (((double) soma_ciclos)) / CLOCKS_PER_SEC); 
+  fprintf (tempo, "Ciclos necessarios: %ld\n", quant_ciclos);
+  fprintf (tempo, "Tempo (segundos): %.50lf\n", (((double) quant_ciclos)) / CLOCKS_PER_SEC); 
 
   ciclos_ini = clock();
   ContagemLeituras ("./projeto_1_dados/genoma_medio.txt", "./projeto_1_dados/pos_genes_grande.csv", "./projeto_1_dados/fragmentos_grande.txt", 
-    "./pos_fragmentos.txt", 10000,"./aatividade_genica.txt");
-  soma_ciclos =+ clock() - ciclos_ini;
+    "./pos_fragmentos.txt", 10000,"./atividade_genica.txt");
+  quant_ciclos = clock() - ciclos_ini;
   fprintf (tempo, "MEDIO\n");
-  fprintf (tempo, "Soma ciclos: %d\n", soma_ciclos);
-  fprintf (tempo, "Tempo (segundos): %.50lf\n", (((double) soma_ciclos)) / CLOCKS_PER_SEC);  
+  fprintf (tempo, "Ciclos necessarios: %ld\n", quant_ciclos);
+  fprintf (tempo, "Tempo (segundos): %.50lf\n", (((double) quant_ciclos)) / CLOCKS_PER_SEC);  
 
   ciclos_ini = clock();
   ContagemLeituras ("./projeto_1_dados/genoma_grande.txt", "./projeto_1_dados/pos_genes_grande.csv", "./projeto_1_dados/fragmentos_grande.txt", 
     "./pos_fragmentos.txt", 10000,"./atividade_genica.txt"); 
-  soma_ciclos =+ clock() - ciclos_ini;
+  quant_ciclos = clock() - ciclos_ini;
   fprintf (tempo, "GRANDE\n");
-  fprintf (tempo, "Soma ciclos: %d\n", soma_ciclos);
-  fprintf (tempo, "Tempo (segundos): %.50lf\n", (((double) soma_ciclos)) / CLOCKS_PER_SEC);
+  fprintf (tempo, "Ciclos necessarios: %ld\n", quant_ciclos);
+  fprintf (tempo, "Tempo (segundos): %.50lf\n", (((double) quant_ciclos)) / CLOCKS_PER_SEC);
 
   fprintf (tempo, "------------------\n");
 
+  fprintf(tempo, "tempo TOTAL (minutos):  %.10lf\n", (((((double)clock() - ciclos_ini_fix)) / CLOCKS_PER_SEC)/(double) 60));
+  fprintf (tempo, "Ciclos necessarios TOTAL: %ld", (clock() - ciclos_ini_fix));
+
+  fclose(tempo);
   return 0;
 }
