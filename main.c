b56c7d3cc6e428262fe04de5542e311a90ea6967
Isaac Soares <isaacsantos.soares@hotmail.com>
2022-06-15 14:05:12 -0300

    Arrumando o main

--- main.c
+++ main.c
@@ -1,23 +1,23 @@
-#define TAM 100
-#include "genoma.h"
-#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+
+#include "genoma.h"
 
 int main(void) {
-
+  //entrada dos caminhos do aruivo de texto e da quantidade de genes
   char* arquivo_genoma = malloc (255 * sizeof(char));
   char* arquivo_pos_genes = malloc (255 * sizeof(char));
   char* arquivo_fragmentos = malloc (255 * sizeof(char));
   char* arquivo_pos_fragmentos = malloc (255 * sizeof(char));
   char* arquivo_saida = malloc (255 * sizeof(char));
   long n_genes;
-
   scanf ("%s %s %s %s %s %ld", arquivo_genoma,  arquivo_pos_genes, arquivo_fragmentos, arquivo_pos_fragmentos, arquivo_saida, &n_genes);
+  
+  //inicia o programa enviando os caminhos dos arquivos recebidos para suas devidas funções
   ContagemLeituras (arquivo_genoma, arquivo_pos_genes, arquivo_fragmentos, 
     arquivo_pos_fragmentos, n_genes, arquivo_saida);
   
+  //libera a memória alocada para as strings
   free (arquivo_genoma);
   free (arquivo_pos_genes);
   free (arquivo_fragmentos);
